dc0073301e625c33df6a75911e7c95e05e2975d6
prakharshriv <prakharshriv@gmail.com>
2020-12-29 15:35:56 +0530

    readme

--- Main.js
+++
@@ -1,8 +0,0 @@
-const service=require('./keyObj')
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-
-// var obj=new service()
--- temp.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs')
-let loc='./data.json'
-var service=require('./keyObj')
-console.log('obj');
-var obj=new service()
-// obj.create('abc',{1:2},(status)=>{
-//     console.log(status);
-// })
